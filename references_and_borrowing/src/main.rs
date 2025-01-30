b3fddd6d7d263c3fe92a2f7a1780f4a4159117a4
Murat Cenk Yilmaz <yilmaz.mcenk@gmail.com>
2025-01-30 14:34:48 +0300

    references and borrowing

---
+++ references_and_borrowing/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+
+    let mut s2 = String::from("hello");
+    
+    change(&mut s2);
+    
+    println!("{s2}");
+}
+
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
